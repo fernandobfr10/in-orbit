eac792ca5212beffc7d1f05f85fc7cd8a1dcac35
Fernando Junior <fernandobfr10@gmail.com>
2024-09-10 12:39:45 -0300

    feat: add and configure biomejs

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -1,1 +1,13 @@
-console.log('Hello World')
+import fastify from 'fastify'
+
+const app = fastify()
+
+const PORT = 3333
+
+app
+  .listen({
+    port: PORT,
+  })
+  .then(() => {
+    console.log('🚀 Server is running on http://localhost:3333')
+  })

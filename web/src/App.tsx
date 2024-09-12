4bd874abe44f9d369e65b88c97d295ae38fc8df3
Fernando Junior <fernandobfr10@gmail.com>
2024-09-12 18:48:09 -0300

    feat: install and configure react query

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,38 @@
-import { Dialog } from './components/ui/Dialog'
+import { useEffect, useMemo, useState } from 'react'
 
 import { CreateGoal } from './components/CreateGoal'
+import { EmptyGoals } from './components/EmptyGoals'
 import { Summary } from './components/Summary'
+import { Dialog } from './components/ui/Dialog'
+
+type SummaryResponse = {
+  completed: number
+  total: number
+  goalsPerDay: Record<
+    string,
+    {
+      id: string
+      title: string
+      completedAt: string
+    }[]
+  >
+}
 
 export function App() {
+  const [summary, setSummary] = useState<SummaryResponse | null>(null)
+
+  useEffect(() => {
+    fetch('http://localhost:3333/summary')
+      .then(response => response.json())
+      .then(data => setSummary(data.summary))
+  }, [])
+
+  const hasSummaryGoals = useMemo(() => summary && summary.total > 0, [summary])
+
   return (
     <Dialog>
-      {/* <EmptyGoals /> */}
-      <Summary />
+      {hasSummaryGoals && <Summary />}
+      {!hasSummaryGoals && <EmptyGoals />}
 
       <CreateGoal />
     </Dialog>
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -2,5 +2,8 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  server: {
+    open: true,
+  },
   plugins: [react()],
 })
